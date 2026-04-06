d3a69e926bc29515338157d64012c9a84c9366e2
ronanb80 <rbrigdale80@gmail.com>
2026-04-06 11:52:35 +0100

    Initial Commit

---
+++ Ulog/UlogApp.swift
@@ -0,0 +1,17 @@
+//
+//  UlogApp.swift
+//  Ulog
+//
+//  Created by Ronan Brigdale on 06/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct UlogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
